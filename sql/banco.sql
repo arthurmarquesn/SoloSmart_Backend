7f8d8aa4b1bc1de39e874875d057223e1de7cbb3
arthurmarquesn <arthur.marques200911@gmail.com>
2025-08-24 21:57:36 -0300

    SQL do Banco

---
+++ sql/banco.sql
@@ -0,0 +1,64 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema solosmart
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema solosmart
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `solosmart` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `solosmart` ;
+
+-- -----------------------------------------------------
+-- Table `solosmart`.`cidades`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `solosmart`.`cidades` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `uf` VARCHAR(2) NULL DEFAULT NULL,
+  `nome` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 136
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `solosmart`.`ufs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `solosmart`.`ufs` (
+  `sigla` CHAR(2) NOT NULL,
+  PRIMARY KEY (`sigla`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `solosmart`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `solosmart`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(100) NOT NULL,
+  `senha` VARCHAR(255) NOT NULL,
+  `uf` CHAR(2) NOT NULL,
+  `cidade` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email` (`email` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
